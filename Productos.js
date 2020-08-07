1ebdfd1d6775c32b23eeffdbeaa85c1ed41d039d
FranciscoY88 <franybarra88@gmail.com>
2020-08-07 20:48:31 -0300

    PopUp0.1

--- Productos.js
+++ Productos.js
@@ -1,3 +1,17 @@
 /////////////////
-//  FRANCISCO  //
+//█████████████//
+//█ FRANCISCO █//
+//█████████████//
 /////////////////
+
+/*
+Historia de usuario: "Como visitante del sitio, si deseo ver ofertas personalizadas, debería poder seleccionar la acción y ser dirigido a la misma".
+
+Se Necesita:
+- Cuando usuario ingresa a la pagina de listado de productos, informarle que tenemos ofertas personalizadas utilizando un popUp (confirm). Si indica que si, redirigirlo a la página correspondiente. Esta acción se dispara bajo las siguientes condiciones:
+
+El usuario accedió a recibir novedades por email
+Nota del arquitecto
+Redirección en JS:
+window.location.replace("ofertasPersonalizadas.html");
+*/
