405f40a3caa7f49998002dda97dfde6546bfcce3
Tynzer <martinsola119@gmail.com>
2020-08-07 20:49:34 -0300

    comentario contacto

---
+++ Contacto.js
@@ -0,0 +1,21 @@
+/*
+Martin
+Historia de usuario: "Como dueño del sitio, quiero mostrarle a los visitantes ofertas especiales de forma de motivarlos a realizar una compra".
+
+Se necesita
+
+En página de contacto, si tenemos el nombre del usuario guardado, mostrarle en pantalla el siguiente mensaje:
+"Estimado/a (Nombre). Gracias por elegirnos! Le obsequiamos el código <PROMO> con su compra".
+Siendo PROMO una selección aleatoria de siguiente lista:
+
+1) "5HOTSALE para obtener un 5% de descuento"
+2) "10HOTSALE para obtener un 10% de descuento"
+3) "15HOTSALE para obtener un 15% de descuento"
+4) "20HOTSALE para obtener un 20% de descuento"
+5) "25HOTSALE para obtener un 25% de descuento"
+
+Nota del arquitecto
+Formula para obtener números aleatorios dentro de un rango:
+Math.floor(Math.random() * MAX) + MIN;
+Donde MAX es el valor máximo del rango y MIN el mínimo
+*/
