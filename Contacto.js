0b64bd2de926c57f172ea86d6bc96a235934e001
Tynzer <martinsola119@gmail.com>
2020-08-08 13:52:49 -0300

    Contacto y promo areeglado

--- Contacto.js
+++ Contacto.js
@@ -34,36 +34,94 @@ let promoArr4= "20HOTSALE para obtener un 20% de descuento";
 let promoArr5= "25HOTSALE para obtener un 25% de descuento";
 
 //let promosgrupoArr= [promoArr1,promoArr2,promoArr3,promoArr4,promoArr5];
-let nombre= "martin";
+//let nombre= "martin";
 
-localStorage.setItem("nombre",nombre); // este es ejemplo
+//localStorage.setItem("nombre",nombre); // este es ejemplo
 
 function codigopromo(nombre,promo){
-    if (localStorage.getItem("nombre") != null){ // en el get item iria la key que pusieron
+    //if (localStorage.getItem("nombre") != null){ // en el get item iria la key que pusieron
     alert("Estimado " + nombre +". Gracias por elegirnos! Le obsequiamos el código "+ promo+ " con su compra")
-    }  
-    return  
-}
-
+}  
+ 
 let ramdonPromo = Math.floor(Math.random() * 5) + 1;
 
-switch (ramdonPromo){
-    case 1:
-    codigopromo(nombre,promoArr1);
-        break;
-    case 2:
-    codigopromo(nombre,promoArr2);
-        break;
-    case 3:
-    codigopromo(nombre,promoArr3);
-        break;
-    case 4:
-    codigopromo(nombre,promoArr4);
-        break;
-    case 5:
-    codigopromo(nombre,promoArr5);
-        break;
+if (localStorage.getItem("nombre") != null){
+    
+   let lsNombre = localStorage.getItem("nombre")
+
+    switch (ramdonPromo){
+        case 1:
+        codigopromo(lsNombre,promoArr1);
+            break;
+        case 2:
+        codigopromo(lsNombre,promoArr2);
+            break;
+        case 3:
+        codigopromo(lsNombre,promoArr3);
+            break;
+        case 4:
+        codigopromo(lsNombre,promoArr4);
+            break;
+        case 5:
+        codigopromo(lsNombre,promoArr5);
+            break;
+    }
 }
+
+//  ERNESTO BUIATTI INICIO
+
+/*Historia de usuario: 
+"Como cliente, quiero personalizar la experiencia del usuario en mi sitio para generar *engagement y potenciar su interés en mis productos y servicios de manera de maximizar la posibilidad de compra"
+
+*(El Engagement puede definirse como el nivel de compromiso que tienen los consumidores y usuarios con una marca)*
+
+**Se necesita:**
+
+- Cuando el usuario ingrese al sitio (desde cualquier página) debemos preguntarle si desea ingresar su nombre y su email. Debemos guardar esa información localmente.
+- Validar que no ingrese datos vacíos. En el caso del email, solo validar que lo ingresado contenga "." y "@"
+- Debemos recordar su decisión para no volver a preguntarle cada vez que ingrese.*/
+
+
+let decision = "";//este parámetro se guarda en local storage por unica vez la primera vez que entra
+let nombre;
+let email = "";
+
+if (!localStorage.getItem ("decision")){// si esta vacio es porque entra por primera vez
+
+    if (confirm("Desea ingresar su nombre y su email")){
+        decision = "si"
+        
+        do{
+            nombre = prompt ("Ingrese su nombre: ");
+            if (nombre != ""){
+                localStorage.setItem ("nombre", nombre);
+                alert ("Nombre guardado correctamente");
+            }
+            else{
+                alert ("No se acepta un campo vacío, vualva a intentarlo por favor");
+            }
+        }while (nombre === "")
+        
+        do{
+            email = prompt ("Ingrese su email: ");
+            if (email != "" && email.indexOf(".")!==-1 && email.indexOf("@")!==-1){
+                localStorage.setItem ("email", email);
+                alert ("Email guardado correctamente");
+            }
+            else{
+                alert ("Ingreso inválido, vualva a intentarlo por favor");
+            }
+        }while (email === "")
+
+    }else{
+        decision = "no"
+       
+    }
+    localStorage.setItem ("decision", decision);
+}
+
+
+//ERNESTO BUIATTI FINAL
 
 
 
