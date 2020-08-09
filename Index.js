b8a53aa7631f53856f21c03b2c42c16083c113ae
ERNESTO <viatsat@gmail.com>
2020-08-09 13:10:44 -0300

    bienvenida

--- Index.js
+++ Index.js
@@ -73,3 +73,25 @@ luis
  
 */
 
+
+
+//Codigo Bienvenida
+
+let texto = "";
+let ahora=new Date();
+let hora= ahora.getHours();
+    if(hora >= 0 && hora<6){
+        texto="Buenos Madrugadas ";  
+    }
+    if(hora >= 6 && hora<12){
+        texto="Buenos días ";  
+    }
+    if(hora>=12 && hora<19){ 
+        texto="Buenas tardes ";
+    }
+    if (hora>=19 && hora<24){ 
+        texto="Buenas noches ";
+    }   //console.log(texto);
+   if (localStorage.getItem("nombre")){
+    alert (texto + localStorage.getItem("nombre"))
+   }
--- bienvenida_nombre.js
+++ bienvenida_nombre.js
@@ -1,23 +1,18 @@
-let nombre= "";
 let texto = "";
 let ahora=new Date();
 let hora= ahora.getHours();
-
     if(hora >= 0 && hora<6){
-        texto="Buenos Madrugadas";  
+        texto="Buenas Madrugadas ";  
     }
     if(hora >= 6 && hora<12){
-        texto="Buenos días";  
+        texto="Buenos días ";  
     }
     if(hora>=12 && hora<19){ 
-        texto="Buenas tardes";
+        texto="Buenas tardes ";
     }
     if (hora>=19 && hora<24){ 
-        texto="Buenas noches";
-    }
-    
-   alert(texto + nombre);
-
-   if (nombre != "" && nombre != null){
-    localStorage.setItem ("nombre", nombre);
-   };
+        texto="Buenas noches ";
+    }   //console.log(texto);
+   if (localStorage.getItem("nombre")){
+    alert (texto + localStorage.getItem("nombre"))
+   }
