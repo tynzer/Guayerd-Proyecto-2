df794a50ab7299e3637f909cab981e98b8ca554f
FranciscoY88 <franybarra88@gmail.com>
2020-08-10 19:27:50 -0300

    PopUp0.9

--- Productos.js
+++ Productos.js
@@ -76,6 +76,29 @@ if (!localStorage.getItem ("decision")){// si esta vacio es porque entra por pri
 
 //ERNESTO BUIATTI FINAL
 
+/////////////////
+//█████████████//
+//█ FRANCISCO █//
+//█████████████//
+/////////////////
+
+/*
+Historia de usuario:
+"Como visitante del sitio, si deseo ver ofertas personalizadas,
+ debería poder seleccionar la acción y ser dirigido a la misma".
+
+Se Necesita:
+- Cuando usuario ingresa a la pagina de listado de productos,
+  informarle que tenemos ofertas personalizadas utilizando un popUp (confirm).
+  Si indica que si, redirigirlo a la página correspondiente.
+  Esta acción se dispara bajo las siguientes condiciones:
+
+El usuario accedió a recibir novedades por email
+Nota del arquitecto
+Redirección en JS:
+window.location.replace("ofertasPersonalizadas.html");
+*/
+
 if(localStorage.getItem("decision", decision)==="si" && localStorage.getItem("email", email)!==null){
     if(confirm("Le informamos que tenemos ofertas especiales, ¿Desea ver?")){
          window.location.replace("ofertasPersonalizadas.html");
