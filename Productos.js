b4b0359d2a6e07c04e6b0b86d679697134bdee14
FranciscoY88 <franybarra88@gmail.com>
2020-08-10 19:08:41 -0300

    PopUp0.7

--- Productos.js
+++ Productos.js
@@ -79,6 +79,7 @@ if (!localStorage.getItem ("decision")){// si esta vacio es porque entra por pri
 if(localStorage.getItem("email", email)!==null && localStorage.getItem("decision", decision)==="si"){
     if(confirm("Le informamos que tenemos ofertas especiales, ¿Desea ver?")){
          window.location.replace("ofertasPersonalizadas.html");
-    }
+    }else{
         decision = "no";
+    }
 }
