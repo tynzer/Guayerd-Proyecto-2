3bb71dca9f05e4cd7072e1f02c6b22d54d56c549
Ernesto <ernesto@gmail.com>
2020-09-08 08:17:43 -0300

    4

--- contacto.js
+++ contacto.js
@@ -1,36 +1,3 @@
-/*Cuando el visitante llena el formulario, enviar los datos al servidor.
-
-URL: https://demo2420474.mockable.io/submitForm (POST)
-
-Formato:
-{name:"", email:"", phone:"",subject:"", message:""}*/
-
-const fnombre = document.querySelector("#nombre");
-const email = document.querySelector("#email");
-const tema = document.querySelector("#tema");
-const mensaje = document.querySelector("#mensaje")
-let form = document.querySelector("#form")
-
-form.addEventListener("submit", formHandler);
-
-function formHandler(event){
-    event.preventDefault();
-    console.log("se cancelo el default");
-    /*fetch('https://demo2420474.mockable.io/submitForm',{
-    method:'POST',
-    body:JSON.stringify({name:fnombre.value, email:email.value, phone:"",subject:tema.value, message:mensaje.value}),
-    headers:{'Content-Type':'application/json'}
-}).then((res)=>{
-    return res.json()
-}).then((res)=>{
-    console.log("FORMULARIO ENVIADO",res)
-});*/
-}
-
-
-
-
-
 /*
 /////////////////
 //█████████████//
@@ -99,5 +66,38 @@ if (localStorage.getItem("nombre") != null) {
 function codigoPromo(nombre, codigo, descuento) {
     alert("Estimado " + nombre + ". Gracias por elegirnos! Le obsequiamos el código: " + codigo + " con un " + descuento + "% del total de su compra.")
 };
+
+
+/*Cuando el visitante llena el formulario, enviar los datos al servidor.
+
+URL: https://demo2420474.mockable.io/submitForm (POST)
+
+Formato:
+{name:"", email:"", phone:"",subject:"", message:""}*/
+
+const fnombre = document.querySelector("#nombre");
+const femail = document.querySelector("#email");
+const tema = document.querySelector("#tema");
+const mensaje = document.querySelector("#mensaje");
+let form = document.querySelector("#form");
+const btnSubmit = document.querySelector("#enviar")
+
+form.addEventListener("submit", formHandler);
+
+function formHandler(event){
+    event.preventDefault();
+    console.log("se cancelo el default");
+    fetch('https://demo2420474.mockable.io/submitForm',{
+    method:'POST',
+    body:JSON.stringify({name:fnombre.value, email:femail.value, phone:"",subject:tema.value, message:mensaje.value}),
+    headers:{'Content-Type':'application/json'}
+}).then((res)=>{
+    return res.json()
+}).then((res)=>{
+    console.log("FORMULARIO ENVIADO",res)
+});
+}
+
+
 
  
