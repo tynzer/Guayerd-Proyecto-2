b206577c6d40760061674b0d21d5ea9923e4d6b4
Tynzer <martinsola119@gmail.com>
2020-09-07 21:17:42 -0300

    cambios prodcutos JSON

--- productos.js
+++ productos.js
@@ -22,8 +22,56 @@ Redirección en JS:
 window.location.replace("ofertasPersonalizadas.html");
 */
 
+if (localStorage.getItem("novedades") === "si") {
+    if (confirm("Le informamos que tenemos ofertas especiales, ¿Desea ver?")) {
+        window.location.replace("ofertasPersonalizadas.html");
+    }
+}
+
+
+
+
+fetch("https://demo2420474.mockable.io/productList").then(function (responsive) {
+    //lee la promesa 
+    return responsive.json();
+    //lee el json
+}).then(function (productos) {
+    //lo que tengo de comentarios es un arreglo?
+    renderProductos(productos)
+});
+
+function renderProductos(productos) {
+    const container = document.querySelector('#container');
+    let fragmentos = "";
+    productos.forEach(function (producto) {
+        console.log(productos)
+        let fragmento = `<div class="bici flexbox">
+                        <img class="flexbox-productos" src="${producto.imgUrl}">
+                        <div class="flexbox-productos">
+                            <h2>${producto.title}</h2>
+                            <p>${producto.description}.</p>
+                        </div>
+                    </div>
+                    <p>Precio: <span id="precio">${producto.price}</span> ${producto.currency} Stock: ${producto.inStock} <span id="descuento">${producto.discountPrice || ""}</span></p>`;
+        fragmentos += fragmento
+        console.log(fragmentos)
+    });
+    container.innerHTML = fragmentos;
+}
+/*     [---POP-UP---]
 if(localStorage.getItem("novedades")==="si"){
     if(confirm("Le informamos que tenemos ofertas especiales, ¿Desea ver?")){
          window.location.replace("ofertasPersonalizadas.html");
     }
-} 
+}
+*/
+
+/*
+Una vez obtenido el listado del servidor, procesarlo y generar el listado en HTML con el estilo propio del sitio.
+
+Agregar en HTML el campo price (Precio)
+Agregar en HTML el campo inStock (Unidades)
+Si llega el campo "discountPrice", mostrar el campo "price" tachado y poner el valor con descuento resaltado
+El criterio estético queda a cargo del equipo respetando el estilo actual de la página.
+*/
+
