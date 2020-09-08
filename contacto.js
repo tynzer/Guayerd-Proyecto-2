1275407fec4531bd7d9381b8ea1ec2f4d14d5476
Tynzer <martinsola119@gmail.com>
2020-09-07 22:01:13 -0300

    cupones

--- contacto.js
+++ contacto.js
@@ -24,30 +24,47 @@ Math.floor(Math.random() * MAX) + MIN;
 Donde MAX es el valor máximo del rango y MIN el mínimo
 */
 
- 
-let promoArr1= "5HOTSALE para obtener un 5% de descuento";  
-let promoArr2= "10HOTSALE para obtener un 10% de descuento";   
-let promoArr3= "15HOTSALE para obtener un 15% de descuento";  
-let promoArr4= "20HOTSALE para obtener un 20% de descuento";   
-let promoArr5= "25HOTSALE para obtener un 25% de descuento";
-let promosgrupoArr= [promoArr1,promoArr2,promoArr3,promoArr4,promoArr5];
+/* 
+let promoArr1 = "5HOTSALE para obtener un 5% de descuento";
+let promoArr2 = "10HOTSALE para obtener un 10% de descuento";
+let promoArr3 = "15HOTSALE para obtener un 15% de descuento";
+let promoArr4 = "20HOTSALE para obtener un 20% de descuento";
+let promoArr5 = "25HOTSALE para obtener un 25% de descuento";
+let promosgrupoArr = [promoArr1, promoArr2, promoArr3, promoArr4, promoArr5];
 let cantidadPromos = promosgrupoArr.length;
-let randomPromo= 0;
+let randomPromo = 0;
 
 //let randomPromo = Math.floor(Math.random() * 5) + 1;
- 
-if (localStorage.getItem("nombre") != null){
-   let lsNombre = localStorage.getItem("nombre") //obtiene el nombre
-   randomSorteo(cantidadPromos)  //ejecuta la funcion del sorteo
-   codigoPromo(lsNombre,promosgrupoArr[randomPromo])
+
+if (localStorage.getItem("nombre") != null) {
+    let lsNombre = localStorage.getItem("nombre") //obtiene el nombre
+    randomSorteo(cantidadPromos)  //ejecuta la funcion del sorteo
+    codigoPromo(lsNombre, promosgrupoArr[randomPromo])
 }
 
-function codigoPromo(nombre,promo){
+function codigoPromo(nombre, promo) {
     //if (localStorage.getItem("nombre") != null){ // en el get item iria la key que pusieron
-    alert("Estimado " + nombre +". Gracias por elegirnos! Le obsequiamos el código "+ promo+ " con su compra")
-}  
- 
-function randomSorteo(cantidadPromos){
+    alert("Estimado " + nombre + ". Gracias por elegirnos! Le obsequiamos el código " + promo + " con su compra")
+}
+
+function randomSorteo(cantidadPromos) {
     randomPromo = Math.floor(Math.random() * cantidadPromos);
     return randomPromo;
 }
+ */
+
+
+if (localStorage.getItem("nombre") != null) {
+    let lsNombre = localStorage.getItem("nombre")
+    fetch("https://demo2420474.mockable.io/getCoupon").then(function (responsive) {
+        return responsive.json();
+    }).then(function (cupones) {
+        codigoPromo(lsNombre, cupones.text, cupones.discountPercentage)
+    })
+};
+
+function codigoPromo(nombre, codigo, descuento) {
+    alert("Estimado " + nombre + ". Gracias por elegirnos! Le obsequiamos el código: " + codigo + " con un " + descuento + "% del total de su compra.")
+};
+
+ 
