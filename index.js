0b5807423c8796d5d62bfb2211421130f64d32e8
lgLuis <luis_unlz@yahoo.com.ar>
2020-09-09 11:29:08 -0300

    luis cbio de cód hacia index

---
+++ index.js
@@ -0,0 +1,29 @@
+//luis
+/*
+Necesitamos que el banner de la página principal se obtenga de un servidor 
+de manera de el equipo de Marketing pueda cambiarlo on-demand.
+URL: http://demo2420474.mockable.io/getHomeBanner (GET)
+Formato del objeto a recibir:
+`
+{
+ title: "",
+ imgUrl: "",
+ link: ""
+}`
+*/
+
+let url="http://demo2420474.mockable.io/getHomeBanner";
+fetch(url)                          //peticion a la URL
+.then(function(response){           //se genera la promesa esperando al serv responda
+    return response.json();         //se recibe la RTA y se tranforma ne JSON
+})                                  //vamos a generar una 2da promesa para...
+.then(function(imagenes){           //trabajar con los datos
+    console.log(imagenes);
+    renderImagenes(imagenes);
+});
+
+
+function renderImagenes(imagenes){
+    let img     =document.querySelector("#banner");
+    img.innerHTML=`<a href = "${imagenes.link}" ><img title = "${imagenes.title}" src = "${imagenes.imgUrl}"></a>`;
+    };
